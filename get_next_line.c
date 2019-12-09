a556e9548d7f9517bf683922f626b6aaa2b54661
Benjot <benjaminmoreau89000@gmail.com>
2019-12-09 17:29:10 +0100

    Boucle cutted

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bemoreau <bemoreau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 10:51:24 by bemoreau          #+#    #+#             */
-/*   Updated: 2019/12/04 15:30:31 by bemoreau         ###   ########.fr       */
+/*   Updated: 2019/12/09 17:12:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_free(char **line)
 {
-	if (!(*line = (char *)malloc(sizeof(char))))
+	if (!(*line = (char *)malloc(sizeof(char) * 1)))
 		return (-1);
 	*line[0] = '\0';
 	return (-1);
@@ -38,24 +38,23 @@ int		ft_charset(char *str)
 
 int my_gnl(int fd, char **line, t_struct *v)
 {
-	while ((v->ret = read(fd, v->buffer, BUFFER_SIZE)) > 0)
+	while ((v->ret = read(fd, v->buffer, BUFFER_SIZE)) > 0 && (v->pos = ft_charset(v->s[fd])) < 0)
 	{
 		if (!(v->tmp = ft_strjoin(v->s[fd], v->buffer)) || !(v->s[fd] = ft_strdup(v->tmp)) || !(v->buffer = ft_strnew(BUFFER_SIZE)))
-			return(ft_free(line));
+			return (ft_free(line));
 	}
+	if (!(v->tmp = ft_strjoin(v->s[fd], v->buffer)) || !(v->s[fd] = ft_strdup(v->tmp)) || !(v->buffer = ft_strnew(BUFFER_SIZE)))
+			return (ft_free(line));
 	free(v->buffer);
 	v->pos = ft_charset(v->s[fd]);
 	if (v->pos >= 0)
 	{
-		*line = ft_substr(v->s[fd], 0, v->pos, 0);
-		v->tmp = ft_substr(v->s[fd], v->pos + 1, ft_strlen(v->s[fd]), 1);
-		v->s[fd] = ft_strdup(v->tmp);
-		return(1);
+		if (!(*line = ft_substr(v->s[fd], 0, v->pos, 0)) || !(v->tmp = ft_substr(v->s[fd], v->pos + 1, ft_strlen(v->s[fd]), 1)) || !(v->s[fd] = ft_strdup(v->tmp)))
+			return (ft_free(line));
+		return (1);
 	}
 	else if (v->ret == 0)
-	{
-		return((*line = ft_strdup(v->s[fd])) ? 0 : -1);
-	}
+		return ((*line = ft_strdup(v->s[fd])) ? 0 : -1);
 	else
 		return (ft_free(line));
 }
@@ -64,14 +63,12 @@ int		get_next_line(int fd, char **line)
 {
 	static t_struct v;
 
-	if (!line || BUFFER_SIZE <= 0 || !(v.buffer = ft_strnew(BUFFER_SIZE)))
+	if (!line || fd < 0 || BUFFER_SIZE <= 0 || !(v.buffer = ft_strnew(BUFFER_SIZE)))
 		return (ft_free(line));
 	if (!v.bool)
-	{
 		if (!(v.s[fd] = ft_strnew(BUFFER_SIZE)))
 			return (ft_free(line));
-		v.bool = 1;
-	}
+	v.bool = 1;
 	if ((v.pos = ft_charset(v.s[fd])) >= 0)
 	{
 		if (!(*line = ft_substr(v.s[fd], 0, v.pos, 0)) || !(v.tmp = ft_substr(v.s[fd], v.pos + 1, ft_strlen(v.s[fd]), 1)))
@@ -80,9 +77,7 @@ int		get_next_line(int fd, char **line)
 		return ((v.s[fd] = ft_strdup(v.tmp)) ? 1 : ft_free(line));
 	}
 	if (v.ret == 0 && ft_strlen(v.s[fd]) != 0 && v.pos == -1)
-	{
 		return ((*line = ft_strdup(v.s[fd])) ? 0 : ft_free(line));
-	}
 	return (my_gnl(fd, line, &v));
 }
 
@@ -109,9 +104,11 @@ int			main()
 	{
 		ft_putendl(line);
 		free(line);
+		printf("%d\n", i);
 	}
 	ft_putendl(line);
+	printf("%d\n", i);
 	free(line);
 	close(fd);
-	return(0);
+	return (0);
 }
